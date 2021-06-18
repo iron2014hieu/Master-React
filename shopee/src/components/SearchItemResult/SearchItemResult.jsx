8aeb66d5aae370fca4e76578bb622f3c2451576f
Duoc <duthanhduoc@gmail.com>
2021-06-18 11:13:50 +0700

    10. Code UI Pagination

--- shopee/src/components/SearchItemResult/SearchItemResult.jsx
+++ shopee/src/components/SearchItemResult/SearchItemResult.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Pagination from '../Pagination/Pagination'
 import ProductItem from '../ProductItem/ProductItem'
 import * as S from './searchItemResult.style'
 
@@ -59,6 +60,7 @@ export default function SearchItemResult() {
         <ProductItem />
         <ProductItem />
       </S.ProductList>
+      <Pagination />
     </div>
   )
 }
